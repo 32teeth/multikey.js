2a6c4c6b7cdf52111a2b9d02c06b0dc15e7bb58b
Eugene Andruszczenko <eugene.andruszczenko@bensimonbyrne.com>
2015-05-06 08:34:12 -0400

    general updates to example

--- controller.js
+++ controller.js
@@ -40,40 +40,40 @@ function controller()
         switch(dir)
         {
           case 1://left
-            ball.style.left = "-20px";
-            ball.style.top = "20px";
+            ball.style.left = "-8px";
+            ball.style.top = "8px";
           break;
           case 2://up
-            ball.style.left = "20px";
-            ball.style.top = "-20px";
+            ball.style.left = "8px";
+            ball.style.top = "-8px";
           break;
           case 3://left up
-            ball.style.left = "-20px";
-            ball.style.top = "-20px";
+            ball.style.left = "-8px";
+            ball.style.top = "-8px";
           break;
           case 4://right
-            ball.style.left = "60px";
-            ball.style.top = "20px";
+            ball.style.left = "24px";
+            ball.style.top = "8px";
           break;
           case 6://right up
-            ball.style.left = "60px";
-            ball.style.top = "-20px";
+            ball.style.left = "24px";
+            ball.style.top = "-8px";
           break;
           case 8://down
-            ball.style.left = "20px";
-            ball.style.top = "60px";
+            ball.style.left = "8px";
+            ball.style.top = "24px";
           break;
           case 9://left down
-            ball.style.left = "-20px";
-            ball.style.top = "60px";
+            ball.style.left = "-8px";
+            ball.style.top = "24px";
           break;
           case 12://right down
-            ball.style.left = "60px";
-            ball.style.top = "60px";
+            ball.style.left = "24px";
+            ball.style.top = "24px";
           break; 
           default:
-            ball.style.left = "20px";
-            ball.style.top = "20px";          
+            ball.style.left = "8px";
+            ball.style.top = "8px";          
           break;            
 
         }
